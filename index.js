33e8b1a3bac5417233765691ab9a2148d5a2547f
dave5361 <143293763+dave5361@users.noreply.github.com>
2023-10-11 19:37:02 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let clickEl = document.getElementById("clickEl")
+let outputEl = document.getElementById("output")
+
+clickEl.addEventListener("click", function() {
+
+    let passwordLength = 12
+
+    function getRandompassword() {
+        randomIndex = Math.floor(Math.random() * characters.length)
+        return characters[randomIndex]
+    }
+
+    function generateRandomPassword() {
+        let randomPassword = ""
+        for (i = 0; i < passwordLength; i++) {
+            randomPassword += getRandompassword()
+        }
+        return randomPassword
+
+    }
+    const generatedPasswordOne = generateRandomPassword()
+    console.log = "Here is a random password: ", generatedPasswordOne 
+
+})
